6c6baf4844a58993c9c463b6a9eb46c4499c0d8a
mazgolom_007 <mazgolom345@gmail.com>
2021-11-09 22:56:00 +0400

    start

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('gender')->nullabe();
+            $table->string('size_penis')->nullabe();
+            $table->string('location')->nullabe();
+            $table->date('birth')->nullabe();
+            $table->string('height')->nullabe();
+            $table->string('weight')->nullabe();
+            $table->text('about_me')->nullabe();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
